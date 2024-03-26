9e5c471cd543c433c0c0b6314a74486779137743
Nikita Semenenko <Nik21081995@yandex.ru>
2024-03-26 21:35:57 +0300

    correct errors

---
+++ README.md
@@ -0,0 +1,1 @@
+![example workflow](https://github.com/Inaba1995/test_selenium/actions/workflows/gradle.yml/badge.svg)
--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,13 +1,14 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,13 +16,17 @@ public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -32,7 +37,7 @@ public class CallbackTest {
 
     @Test
     void shouldTestFields() throws InterruptedException {
-        driver.get("http://0.0.0.0:9999");
+        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Никита Иванов");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79271234567");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
